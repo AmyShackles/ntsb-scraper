8e3eed993d420483acd0b17162df025b631573b1
Amy Shackles <amyshackles@gmail.com>
2020-12-03 22:28:26 -0800

    Modified scraper to enable batching

--- scraper.js
+++ scraper.js
@@ -1,24 +1,39 @@
 const puppeteer = require("puppeteer");
+const { urls } = require("./airplaneNTSBnumbers");
+const fs = require("fs");
 
-const url =
-  "https://www.asias.faa.gov/apex/f?p=100:17:::NO::AP_BRIEF_RPT_VAR:CEN20LA412";
+const commandLineArguments = process.argv.slice(2);
+const fileName = commandLineArguments[0];
+const reportNumberIndexStart = commandLineArguments[1];
+const reportNumberIndexEnd = commandLineArguments[2];
+const reportNumbers = urls.slice(reportNumberIndexStart, reportNumberIndexEnd);
 
 class Crawler {
-  constructor() {
-    this.url = url;
-  }
+  constructor() {}
 
   crawl() {
     (async () => {
-      console.log(await this.execute());
+      console.log("running");
+      const browser = await puppeteer.launch({ timeout: 0 });
+      const page = await browser.newPage();
+      for (const reportNumber of reportNumbers) {
+        const url = `https://www.asias.faa.gov/apex/f?p=100:18:::NO::AP_BRIEF_RPT_VAR:${reportNumber}`;
+        console.log(url);
+        try {
+          await page.goto(url);
+          fs.writeFileSync(fileName, JSON.stringify(await this.execute(page)), {
+            flag: "a",
+          });
+        } catch (err) {
+          await browser.close();
+          console.error(error);
+        }
+      }
+      await browser.close();
     })();
   }
 
-  async execute() {
-    console.log("running");
-    const browser = await puppeteer.launch({ dumpio: true });
-    const page = await browser.newPage();
-    await page.goto(this.url);
+  async execute(page) {
     const t = await page.evaluate((sel) => {
       const tables = document.querySelectorAll("table");
       let parsedJSON = {};
@@ -75,10 +90,9 @@ class Crawler {
       });
       return parsedJSON;
     });
-    await browser.close();
     return t;
   }
 }
 console.log("started");
 
-new Crawler(url).crawl();
+new Crawler().crawl();
